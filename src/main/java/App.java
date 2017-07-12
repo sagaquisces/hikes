95b2955f4bbb58c30018442c2c9f36d3bb278484
Joel Bakken and Michael Dunlap <michael@>
2017-07-12 14:10:31 -0700

    add multiple .vtl files and user interface logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -30,7 +30,7 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/hikes/new", (request, response) -> {
+    get("/new-hike", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/hike-form.vtl");
       return new ModelAndView(model, layout);
